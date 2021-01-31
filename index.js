a8a0b9a06dfe5689f7b62285ad17cd34b6aa3ad6
Cholakyan <ncholak@iastate.edu>
2021-01-30 23:16:38 -0600

    init first

---
+++ index.js
@@ -0,0 +1,7 @@
+let http = require('http');
+let mongo = require('mongodb');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
