d6349b3b389d422a3fefb1bc6cc226ba58395978
Cholakyan <ncholak@iastate.edu>
2021-02-02 22:08:28 -0600

    change model

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const vocab = require('./lezoovocab')
 const express = require('express')
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 // Routes which should handle request
 app.get("/test", (req, res, next) => {
     res.json(["Orange","Apple","Watermellon"]);
@@ -12,6 +15,13 @@ app.get("/words", (req, res, next) => {
     res.json(vocab.reverse())
 });
 
+// Routes which should handle request
+app.post("/words", (req, res) => {
+    let day = req.body.day
+    let start = day > 5 ? day - 5 : 0
+    // res.sendStatus(200)
+    res.json(vocab.slice(start, day).reverse())
+});
 
 //export app
 module.exports = app;
--- lezoovocab.js
+++ lezoovocab.js
@@ -4,45 +4,35 @@ const vocab = [
         "translation": "language, tonge",
         "description": "",
         "pronounce": "lezoo",
-        "isNoun": true,
-        "isVerb": false,
-        "isAdjective": false
+        "grammar": "noun"
     },
     {
         "word": "ձուկ",
         "translation": "fish",
         "description": "",
         "pronounce": "dzuk",
-        "isNoun": true,
-        "isVerb": false,
-        "isAdjective": false
+        "grammar": "noun"
     },
     {
         "word": "բանալի",
         "translation": "key",
         "description": "",
         "pronounce": "banali",
-        "isNoun": true,
-        "isVerb": false,
-        "isAdjective": false
+        "grammar": "noun"
     },
     {
         "word": "փափուկ",
         "translation": "soft",
         "description": "",
         "pronounce": "papook",
-        "isNoun": false,
-        "isVerb": false,
-        "isAdjective": true
+        "grammar": "adjective"
     },
     {
         "word": "գեղեցիկ",
         "translation": "beautiful",
         "description": "",
         "pronounce": "geghets’ik",
-        "isNoun": false,
-        "isVerb": false,
-        "isAdjective": true
+        "grammar": "adjective"
     }   
 ]
 
